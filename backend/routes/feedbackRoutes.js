4b559ceddc03d7cbaa8038b236bccc2471517c4d
Dhruvil Patel <dhruvilpatel.m@gmail.com>
2025-02-03 13:35:45 +0530

    Delete backend directory

--- backend/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitFeedback } = require("../controllers/feedbackController");
-
-// POST route for submitting feedback
-router.post("/feedback", submitFeedback);
-
-module.exports = router;
