838a01e4389caf36a749bfc5c9dc51f43dd6f7be
Dhruvil <dhruvilpatel.m@gmail.com>
2025-02-25 14:02:33 +0530

    Add routing with React Router and update styles for the application

--- frontend/vite_app/src/App.jsx
+++ frontend/vite_app/src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import FeedbackForm from "./components/FeedbackForm.jsx";
-import LandingPage from "./components/LandingPage.jsx" ;
-import IELTSMaster from "./components/Practice.jsx" ;
-import IELTSReadingPractice from "./components/Reading-test.jsx" ;
-import ReadingTest from "./components/ReadingTest.jsx" ;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // Navigation Bar
+import LandingPage from "./components/LandingPage";
+import Practice from "./components/Practice";
+import ReadingTest from "./components/ReadingTest";
+import WritingTest from "./components/WritingTest";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   return (
-    <div>
-      {/* <h1>IELTS Preparation Feedback</h1>
-      <FeedbackForm /> */}
-      {/* <LandingPage /> */}
-      {/* <IELTSMaster /> */}
-      {/* <IELTSReadingPractice /> */}
-      <ReadingTest />
-    </div>
+    <Router>
+      <Navbar /> {/* Navigation Bar */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/practice" element={<Practice />} />
+        <Route path="/reading-test" element={<ReadingTest />} />
+        <Route path="/writing-test" element={<WritingTest />} />
+        <Route path="/feedback" element={<FeedbackForm />} />
+      </Routes>
+    </Router>
   );
 }
 
