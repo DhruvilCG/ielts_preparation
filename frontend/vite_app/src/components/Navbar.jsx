04cd98209027fe2fcf736b19d80f60777b2b9acb
Dhruvil <dhruvilpatel.m@gmail.com>
2025-02-25 13:55:44 +0530

    Add Navbar component with styling and navigation links

---
+++ frontend/vite_app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Add CSS for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/practice">Practice</Link></li>
+        <li><Link to="/reading-test">Reading Test</Link></li>
+        <li><Link to="/writing-test">Writing Test</Link></li>
+        <li><Link to="/feedback">Feedback</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
